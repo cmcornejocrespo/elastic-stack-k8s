0817ca5d3b787844c1232b0230a99fc8d665f555
Carlos María Cornejo Crespo <cmcornejo@atsistemas.com>
2019-02-28 19:36:03 +0100

    upgraded to 6.6.1

--- teardown.sh
+++ teardown.sh
@@ -3,5 +3,6 @@
 az aks get-credentials --resource-group aks-pets --name aks-pets --overwrite-existing
 
 kubectl delete -Rf k8s-specs/
+sleep 30
 kubectl delete pvc elasticsearch-data-elasticsearch-0 -n pets
 kubectl delete namespace pets
