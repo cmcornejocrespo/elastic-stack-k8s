e0c78651be5f22a1fe5ffb0cc1f47013f4c9d727
Carlos María Cornejo Crespo <cmcornejo@atsistemas.com>
2019-04-08 13:20:44 +0200

    changed namespace housekeeping

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-az aks get-credentials --resource-group aks-pets --name aks-pets --overwrite-existing
+az aks get-credentials --resource-group elastic-aks --name elastic-aks --overwrite-existing
 kubectl create clusterrolebinding kubernetes-dashboard --clusterrole=cluster-admin  --serviceaccount=kube-system:kubernetes-dashboard
 
-kubectl create namespace pets
+kubectl create namespace elastic
 kubectl create -Rf k8s-specs/
--- teardown.sh
+++ teardown.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-az aks get-credentials --resource-group aks-pets --name aks-pets --overwrite-existing
+az aks get-credentials --resource-group elastic-aks --name elastic-aks --overwrite-existing
 
 kubectl delete -Rf k8s-specs/
 sleep 30
-kubectl delete pvc elasticsearch-data-elasticsearch-0 -n pets
-kubectl delete namespace pets
+kubectl delete pvc elasticsearch-data-elasticsearch-0 -n elastic
+kubectl delete namespace elastic
