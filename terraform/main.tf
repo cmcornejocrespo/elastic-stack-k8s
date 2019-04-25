94b840beff54e88fcacc6daf997685beccec0ccd
Carlos María Cornejo Crespo <cmcornejo@atsistemas.com>
2019-04-25 12:25:04 +0200

    updated tags

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "rg" {
   location = "${var.location_extended}"
 
   tags {
-    description = "elastic demo aks rg"
+    description = "elastic meetup BCN - elastic demo aks rg"
   }
 }
 
